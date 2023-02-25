ba28ba43f084eaeb2855592186157c434afaf2d0
ahmadreza-n <ahmadrezaanazari@gmail.com>
2023-02-25 16:39:48 -0700

    removed extra files, subprocvenv working...

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
-import logging
-logging.basicConfig(
-      format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO, datefmt='%m-%d %H:%M:%S')
-import numpy as np
-from stable_baselines3 import PPO
-from stable_baselines3.ppo import MlpPolicy
-from imitation.algorithms.adversarial.gail import GAIL
-from imitation.rewards.reward_nets import BasicRewardNet
-from utils.read_data import read_data
-from utils.get_env import get_venv
-from utils.export_onnx import export_model
-
-
 if __name__ == "__main__":
+  # on linux, by default it's fork -- we change it to spawn for some reason
+  from multiprocessing import set_start_method, cpu_count
+  set_start_method('spawn')
+  import logging
+  logging.basicConfig(
+      format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO, datefmt='%m-%d %H:%M:%S')
+  import numpy as np
+  from stable_baselines3 import PPO
+  from stable_baselines3.ppo import MlpPolicy
+  from imitation.algorithms.adversarial.gail import GAIL
+  from imitation.rewards.reward_nets import BasicRewardNet
+  from utils.read_data import read_data
+  from utils.get_env import get_venv
+  from utils.export_onnx import export_model
+
   logger = logging.getLogger()
+  logger.info('reading the data...')
   data = read_data()
   rng = np.random.default_rng(0)
-  venv = get_venv(1, rng, logger)
+  logger.info(f'creating venv with cpu count {cpu_count()}')
+  venv = get_venv(cpu_count(), rng, logger)
 
   try:
+    logger.info('creating the learner, policy, and trainer...')
     learner = PPO(env=venv, policy=MlpPolicy)
     reward_net = BasicRewardNet(
         venv.observation_space,
@@ -25,12 +30,14 @@ if __name__ == "__main__":
     )
     gail_trainer = GAIL(
         demonstrations=data,
-        demo_batch_size=64,
+        demo_batch_size=512,
         venv=venv,
         gen_algo=learner,
         reward_net=reward_net
     )
-    gail_trainer.train(gail_trainer.gen_train_timesteps * 20)
+    logger.info('begin training...')
+    gail_trainer.train(gail_trainer.gen_train_timesteps * 50)
+    logger.info('exporting to onnx...')
     export_model(gail_trainer.policy, venv.observation_space, 'test3.onnx')
   finally:
     logger.info('closing venv...')
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "starting process..."
+nohup /home/ahmadreza-n/miniconda3/envs/irl/bin/python /home/ahmadreza-n/Documents/imitation-crp/main.py > ./output.log &
